b39674c5f74057a5927c06eca092c9956b04e4f8
jarmol <jjlammi@hotmail.fi>
2014-08-23 11:56:16 +0300

    The initial version

--- mysite/views.py
+++ mysite/views.py
@@ -1,18 +1,18 @@
-#import django.shortcuts
 from django.template.loader import get_template
 from django.template import Template, Context
 from django.http import HttpResponse
 import datetime
 
 def hello(request):
-    return HttpResponse("Hello Python-Django world !")
+    message = "Your browser is %s and IP %s"
+    ua = request.META['HTTP_USER_AGENT']
+    ub = request.META['REMOTE_ADDR']
+    return HttpResponse(message % (ua, ub))
 
 def current_datetime(request):
     now = datetime.datetime.now()
     t = get_template('current_datetime.html')
     html = t.render(Context({'current_date': now}))
-    #Ei toiminut shortcut renderin avulla
-    #html = render(request, 'current_datetime.html', {'current_date': now})
     return HttpResponse(html)
 
 def hours_ahead(request, offset):
